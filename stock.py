4fc691460e3852338a0c2d6fdc69d9d8e0c48f82
bx <xiang.bao.frank@gmail.com>
2016-05-08 10:55:18 +0800

    get history data

--- stock.py
+++ stock.py
@@ -1,2 +1,22 @@
+# -*- coding: utf8 -*-
 import tushare as ts
 from sqlalchemy import create_engine
+from datetime import datetime
+
+stock_basics = ts.get_stock_basics()
+
+db = create_engine('sqlite:///stock.db')
+
+stock_basics['date'] = datetime.now().date()
+stock_basics.to_sql('stock_basics', db)
+# print(stock_basics.head(10))
+# print(datetime.now().date())
+codes = list(stock_basics.index)
+counter = 1
+for code in codes:
+    # 取得所有日线数据并存入数据库
+    his_data = ts.get_hist_data(code, retry_count=20, pause=3)
+    his_data['code'] = code
+    print(code, counter, '/', len(codes))
+    counter += 1
+    his_data.to_sql('history_data', db, if_exists='append')
