bf9e265aba6264bfe3d1323e3e9b36f62239733a
bx <xiang.bao.frank@gmail.com>
2016-06-19 14:33:05 +0800

    get history data

--- stock.py
+++ stock.py
@@ -2,18 +2,25 @@
 import tushare as ts
 import pandas as pd
 import numpy as np
+import talib as ta
 
 print(ts.__version__)
 
-report = ts.get_report_data(2016, 1)
-report['year'] = 2016
-report['quarter'] = 1
-for year in range(2005, 2016):
-    for quarter in range(1, 5):
-        print(year, quarter)
-        r = ts.get_report_data(year, quarter)
-        r['year'] = year
-        r['quarter'] = quarter
-        report = report.append(r, ignore_index=True)
-report.sort_values(by='year')
-report.to_csv('report.csv')
+
+def get_reports():
+    report = ts.get_report_data(2016, 1)
+    report['year'] = 2016
+    report['quarter'] = 1
+    for year in range(2005, 2016):
+        for quarter in range(1, 5):
+            print(year, quarter)
+            r = ts.get_report_data(year, quarter)
+            r['year'] = year
+            r['quarter'] = quarter
+            report = report.append(r, ignore_index=True)
+    report.sort_values(by='year')
+    report.to_csv('report.csv')
+
+data = ts.get_hist_data('000681')
+
+close = data.close
