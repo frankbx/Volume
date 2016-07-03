8ad969dc3571d9747803702056a79e7d3d69c7b9
bx <xiang.bao.frank@gmail.com>
2016-07-03 23:43:14 +0800

    rename file

---
+++ dataAcquisition.py
@@ -0,0 +1,40 @@
+# -*- coding: utf8 -*-
+import tushare as ts
+
+print(ts.__version__)
+
+
+def get_reports():
+    report = ts.get_report_data(2016, 1)
+    report['year'] = 2016
+    report['quarter'] = 1
+    for year in range(2005, 2016):
+        for quarter in range(1, 5):
+            print(year, quarter)
+            r = ts.get_report_data(year, quarter)
+            r['year'] = year
+            r['quarter'] = quarter
+            report = report.append(r, ignore_index=True)
+    report.sort_values(by='year')
+    report.to_excel('report.xlsx')
+
+
+def get_hist_data(code, ktype='D'):
+    data = ts.get_hist_data(code, ktype=ktype)
+    data = data.sort_index(axis=0)
+    data.to_csv(code + '-' + ktype + '.csv')
+
+
+def get_sz_data():
+    sz = ts.get_h_data('399106', start='2000-01-01', index=True)
+    # sz.to_csv('sz.csv')
+    return sz
+
+
+def get_sh_data():
+    sh = ts.get_h_data('000001', start='2000-01-01', index=True)
+    # sh.to_csv('sh.csv')
+    return sh
+
+
+get_sh_data()
