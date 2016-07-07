36aa50ada6b6e068e0406c42c1c80fad2040c2fe
bx <xiang.bao.frank@gmail.com>
2016-07-08 00:26:01 +0800

    rename file

--- pillar.py
+++ pillar.py
@@ -1,11 +1,10 @@
 # -*- coding: utf8 -*-
 import tushare as ts
 
-his_data = ts.get_hist_data('600493', retry_count=20, pause=3)
-his_data['code'] = '600493'
-his_data.to_pickle('600493.dat')
+his_data = ts.get_hist_data('000681', retry_count=20, pause=3)
+his_data['code'] = '000681'
+# his_data.to_pickle('600493.dat')
 # print(his_data.volume.head(5))
-print(his_data.shape)
-df = his_data.copy().loc[:, ['volume']]
-print(type(df))
-print(type(his_data.volume))
+# print(his_data.shape)
+df = his_data.loc[:, ['open', 'close', 'low', 'high', 'p_change', 'volume']]
+print(df[df.p_change > 5])
