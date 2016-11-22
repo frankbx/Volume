80f99e2458639e3aaca397e94822b35bce0e9646
Frank Bao <xiang.bao.frank@gmail.com>
2016-11-22 19:43:49 +0800

    update

--- dataAcquisition.py
+++ dataAcquisition.py
@@ -2,7 +2,7 @@
 import threading
 from math import ceil
 from time import ctime, time
-
+import os
 import tushare as ts
 
 from volumeUtils import *
@@ -117,6 +117,26 @@ def get_stock_data(code, ktype='D', start=None, end=None):
 def get_stock_basics():
     basics = ts.get_stock_basics()
     basics.to_csv("./basics.csv", encoding='utf8')
+
+def transform_tongdaxin_data(original_file,transformed_file):
+    data = pd.read_csv(original_file,
+                   header=None,names=['date','time','open','high','low','close','volume','amount'],
+                   encoding='cp936', dtype={'time':np.str})[:-1]
+    if os.path.exists(transformed_file):
+        existing_data = pd.read_csv(transformed_file,dtype={'time':np.str})
+        r,c = existing_data.shape
+        latest_date = existing_data.date[r-1]
+        latest_time = existing_data.time[r-1]
+#         print(latest_date)
+#         criterion1 = (data.date==latest_date and data.time>latest_time)
+#         criterion2 = data.date>latest_date
+        delta1 = data[data.date==latest_date][data.time>latest_time]
+        delta2 = data[data.date>latest_date]
+#         print(delta)
+        delta1.to_csv(transformed_file,mode='a', header=None,index=False)
+        delta2.to_csv(transformed_file,mode='a', header=None,index=False)
+    else:
+        df = data.to_csv(transformed_file,index=False,encoding='utf-8',dtype={'time':np.str})
 
 
 def get_tick_data(code,start=None,end=None):
--- dataLoader.py
+++ dataLoader.py
@@ -9,21 +9,20 @@ from volumeUtils import *
 class DataLoader:
     def __init__(self, start=None, end=None, ktype='D'):
         self.big_data = None
-        print(os.path.abspath(os.path.curdir))
         data_dir = DATA_DIR_DICT[ktype]
-        # data_dir = os.path.join(os.path.abspath(os.path.curdir), data_dir)
         print(data_dir)
         d = []
-        for p, dir_name, filenames in os.walk(data_dir):
-            for filename in filenames:
-                print(os.path.join(p, filename))
-                data = pd.read_csv(os.path.join(p, filename), encoding='cp936')
+        for parent_dir_name, dir_names, file_names in os.walk(data_dir):
+            for filename in file_names:
+                print(os.path.join(parent_dir_name, filename))
+                data = pd.read_csv(os.path.join(parent_dir_name, filename), encoding='cp936')
                 # if self.big_data is None:
                 #     self.big_data = [data]
                 # else:
                 #     self.big_data.append(data)
                 d.append(data)
-        self.big_data = pd.concat(d, ignore_index=True)
+        # self.big_data = pd.concat(d, ignore_index=True)
+        self.big_data = d
 
     def get_data(self):
         return self.big_data
@@ -35,7 +34,7 @@ if __name__ == '__main__':
 
     dl = DataLoader(ktype='m')
     d = dl.get_data()
-    print(d.tail(10))
+    print(d[-1])
     end = time()
     print('End at:', ctime())
-    print('Duration:', round(end - start, 2))
+    print('Duration:', round(end - start, 2), 'seconds')
