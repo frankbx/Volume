201081250ecfa9b55dd89136027819ef16eab9e7
Bao Xiang <xiang.bao.frank@gmail.com>
2016-12-18 14:52:59 +0800

    update

--- dataAcquisition.py
+++ dataAcquisition.py
@@ -40,7 +40,7 @@ def get_all_data(ktype='D', ):
     directory = DATA_DIR_DICT[ktype]
     if not os.path.exists(directory):
         os.mkdir(directory)
-    chuncks = split_into_chunck(df.code, 20)
+    chuncks = split_into_chunck(df.code, 40)
     threads = list()
     for i in range(len(chuncks)):
         th = threading.Thread(target=process, args=(chuncks[i], ktype))
@@ -71,35 +71,45 @@ def get_stock_data(code, ktype='D'):
     if os.path.exists(filename):
         # get the latest date
         # print(filename)
-        existing_data = pd.read_csv(filename)
+        try:
+            existing_data = pd.read_csv(filename)
+        except pd.io.common.EmptyDataError as e:
+            print(filename)
         row, col = existing_data.shape
         latest_date = existing_data.date[row - 1]
         # retrieve data from the latest date
-        data = ts.get_h_data(code=code, start=latest_date, ktype=ktype, retry_count=30, pause=2)
-        r, c = data.shape
-        # discard duplicated data of the last day if there's more than 1 row
-        if r > 1:
-            # Locate by integer, not index
-            delta_data = data.iloc[:r - 1].copy()
-            # The data is sorted so that the latest data at the bottom of the file.
-            # It's easier to append future data while keep the ascending order of date
-            delta_data.sort_index(axis=0, inplace=True)
-            # print(delta_data)
-            # Append data to the file
-            delta_data.to_csv(filename, mode='a', header=None)
-            print(code, 'updated')
-    else:
-        basics = pd.read_csv('basics.csv', dtype={'code': np.str})
-        start_date = basics.ix[code]['timeToMarket']
-        # Create the data file directly
-        data = ts.get_h_data(code=code, ktype=ktype, start=start_date,
-                                retry_count=20, pause=1)
-        # Data can be None if it's a new stock
+        data = ts.get_k_data(code=code, start=latest_date, retry_count=30, pause=2)
         if data is not None:
+            r, c = data.shape
+            # discard duplicated data of the last day if there's more than 1 row
+            if r > 1:
+                # Locate by integer, not index
+                delta_data = data.iloc[1:r].copy()
+                # The data is sorted so that the latest data at the bottom of the file.
+                # It's easier to append future data while keep the ascending order of date
+                delta_data.sort_index(axis=0, inplace=True)
+                # print(delta_data)
+                # Append data to the file
+                delta_data.to_csv(filename, mode='a', header=None, index=False)
+                print(code, 'updated')
+    else:
+        basics = pd.read_csv('./basics.csv', dtype={'code': np.str})
+        # basics = basics[basics.timeToMarket != 0]
+        basics.index = basics.code
+        start_date = str(basics.ix[code]['timeToMarket'])
+        if start_date != '0':
+            start_date = start_date[0:4] + '-' + start_date[4:6] + '-' + start_date[6:8]
+        else:
+            start_date = None
+        # print(start_date)
+        # Create the data file directly
+        data = ts.get_k_data(code=code, start=start_date, retry_count=20, pause=1)
+        # Data can be None if it's a new stock
+        if data is not None and len(data) >= 1:
             # The data is sorted so that the latest data at the bottom of the file.
             # It's easier to append future data while keep the ascending order of date
             data.sort_index(axis=0, inplace=True)
-            data.to_csv(filename)
+            data.to_csv(filename, index=False)
             print(code, 'created')
 
 
