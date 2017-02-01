6e514767a9363a6f06644520a7789a9789bc6556
Frank Bao <xiang.bao.frank@gmail.com>
2017-02-01 14:51:23 +0800

    update

--- pair_correlation.py
+++ pair_correlation.py
@@ -1,6 +1,8 @@
 import math
+import numpy as np
 
 import pandas as pd
+from time import ctime, time
 
 
 def code_2_file(code):
@@ -28,4 +30,29 @@ def pair_correlation(code1, code2, start='2014-12-31'):
 
 
 if __name__ == '__main__':
-    print(pair_correlation('600084', '601668'))
+    # print(pair_correlation('600084', '601668'))
+    basics = pd.read_csv('./basics.csv', dtype={'code': np.str})
+    basics = basics[basics.timeToMarket < 20141231]
+    basics = basics[basics.timeToMarket > 0]
+    codes = list(basics.code)
+    codes.sort()
+    # print(codes)
+    length = len(codes)
+    print(length)
+    results = []
+    start = time()
+    print('Start at:', ctime())
+    for i in range(0, length):
+        # print(codes[i])
+        for j in range(i + 1, length):
+            corr = pair_correlation(codes[i], codes[j])
+            results.append({'code1': codes[i], 'code2': codes[j], 'corr': corr})
+        # if i == 1:
+        #     # print(results)
+        #     break
+    df = pd.DataFrame(results)
+    df.to_csv('results.csv', index=False)
+    # print(len(results))
+    end = time()
+    print('End at:', ctime())
+    print('Duration:', round(end - start, 2), 'seconds')
